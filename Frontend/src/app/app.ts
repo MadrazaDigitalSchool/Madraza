67ea59e545ae1d31756d905706b849add174ee8d
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2026-05-01 22:14:19 +0200

    Probar el footer y crear íconos de redes sociales

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
+import { FooterComponent } from "./shared/components/footer/footer";
 
 /**
  * Componente raíz de la aplicación Madraza
@@ -9,7 +10,7 @@ import { HeaderComponent } from './shared/components/header/header';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- Frontend/src/app/shared/components/footer/footer.ts
+++ Frontend/src/app/shared/components/footer/footer.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrls: ['./footer.scss']
 })
 export class FooterComponent {
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    const icons = ['google', 'facebook', 'youtube', 'instagram', 'linkedin'];
 
+    icons.forEach(icon => {
+      this.iconRegistry.addSvgIcon(
+        icon,
+        // Al estar en public/assets/icons, la ruta del navegador es /assets/icons/
+        this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`)
+      );
+    });
+  }
 }
