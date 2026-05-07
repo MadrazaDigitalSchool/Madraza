17257e507c7e406bdb9769b3676aa041da38e5c4
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2026-05-07 19:52:27 +0200

    Corrección de btns en precios

--- Frontend/src/app/features/info/precios/precios.ts
+++ Frontend/src/app/features/info/precios/precios.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../../core/services/auth';
@@ -8,7 +8,7 @@ import { AuthService } from '../../../core/services/auth';
 @Component({
   selector: 'app-precios',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './precios.html',
   styleUrl: './precios.scss'
 })
